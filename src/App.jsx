ddf9ee2e70370e355e9af074fa1a18a322c938f9
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2023-10-09 20:35:12 +0200

    fix css styles

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,17 @@ const App = () => {
   } = useFetch();
 
   return (
-    <div className='m-4 rounded-xl flex gap-5 text-white'>
-      <div className='rounded-xl w-[200px] h-fit bg-[#33374f] hidden sm:block shadow-md shadow-black'>
-        <h2 className='p-3 text-xl  font-bold text-center shadow-md shadow-black rounded-lg'>
-          Genres
-        </h2>
+    <div className='m-4 flex gap-5 text-white'>
+      <div className='rounded-xl w-[200px] h-fit bg-[#33374f] hidden sm:block'>
+        <h2 className='p-3 text-xl  font-bold text-center '>GENRES</h2>
         {genres
           .sort((a, b) => (a.name < b.name ? -1 : 1))
           .map(genre => (
             <div
               key={genre.id}
-              className={`mx-2 my-3 hover:bg-white hover:text-[#1b1d29] hover:shadow-md hover:shadow-black cursor-pointer font-semibold rounded-lg p-2 ${
+              className={`mx-2 my-3 hover:bg-white hover:text-[#1b1d29] hover:shadow-[#1b1d29] hover:shadow cursor-pointer font-semibold rounded-lg p-2 ${
                 select === genre.id &&
-                'bg-white text-black shadow-black shadow-md'
+                'bg-white text-black shadow-[#1b1d29] shadow'
               }`}
               onClick={() => {
                 handleSelect(genre.id);
@@ -36,18 +34,18 @@ const App = () => {
             </div>
           ))}
       </div>
-      <div className='w-full rounded-xl bg-[#33374f] shadow-md shadow-black'>
-        <div className='p-3 text-xl font-bold text-center flex items-center justify-between shadow-md shadow-black rounded-lg'>
+      <div className='w-full rounded-xl bg-[#33374f]'>
+        <div className='p-3 text-xl font-bold text-center flex items-center justify-between '>
           <BiSolidLeftArrow className='cursor-pointer' onClick={handlePrev} />
-          <h2>Games</h2>
+          <h2>GAMES</h2>
           <BiSolidRightArrow className='cursor-pointer' onClick={handleNext} />
         </div>
-        <div className='grid base:grid-cols sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 content-center p-2 w-full justify-center font-semibold'>
+        <div className='grid base:grid-cols sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 content-center p-2 w-full justify-center font-semibold '>
           {filteredGames.length > 0 ? (
             filteredGames
               .sort((a, b) => (a.name < b.name ? -1 : 1))
               .map(game => (
-                <div key={game.id} className='m-2 '>
+                <div key={game.id} className='m-2 rounded-xl'>
                   <p className='mb-2'>
                     {game.name.length > 30
                       ? game.name.slice(0, 30) + '...'
@@ -57,7 +55,7 @@ const App = () => {
                   <img
                     src={game.background_image}
                     alt={game.name}
-                    className='h-[200px] w-[320px] rounded-xl object-cover transition-all ease-in-out duration-200 hover:border-[2px] hover:border-white border-[2px] border-transparent hover:scale-[103%] shadow-md shadow-black'
+                    className='h-[200px] w-[320px] rounded-xl object-cover shadow-[#1b1d29] shadow transition-all ease-in-out duration-100 hover:scale-[103%]'
                   />
                 </div>
               ))
