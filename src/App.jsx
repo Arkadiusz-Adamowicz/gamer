b8954d7e70d694d1c7764a6c12cb9709799d42f0
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2023-10-09 19:10:01 +0200

    fix env

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,16 @@
-/* eslint-disable no-undef */
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { BiSolidLeftArrow, BiSolidRightArrow } from 'react-icons/bi';
-import.meta.env.VITE_RAWG_KEY;
+import useFetch from './hooks/useFetch';
 
 const App = () => {
-  const [games, setGames] = useState([]);
-  const [filteredGames, setFilteredGames] = useState([]);
-  const [page, setPage] = useState(1);
-  const [select, setSelect] = useState(0);
-  const genres = [
-    {
-      id: 2,
-      name: 'Shooter',
-    },
-    {
-      id: 3,
-      name: 'Action',
-    },
-    {
-      id: 4,
-      name: 'Adventure',
-    },
-    {
-      id: 5,
-      name: 'RPG',
-    },
-    {
-      id: 7,
-      name: 'Puzzle',
-    },
-    {
-      id: 51,
-      name: 'Indie',
-    },
-    {
-      id: 59,
-      name: 'Multiplayer',
-    },
-    {
-      id: 83,
-      name: 'Platformer',
-    },
-  ];
-
-  useEffect(() => {
-    fetchGames();
-  }, [page]);
-
-  const handleSelect = name => {
-    setSelect(name);
-  };
-
-  const filterGames = genre => {
-    const { id, name } = genre;
-    name === 'All'
-      ? setFilteredGames(games)
-      : setFilteredGames(
-          games.filter(
-            game => game.genres && game.genres.find(genre => genre.id === id)
-          )
-        );
-  };
-
-  const handlePrev = () => {
-    page > 0 && setPage(page => page - 1);
-  };
-
-  const handleNext = () => {
-    setPage(page => page + 1);
-  };
-
-  const fetchGames = async () => {
-    try {
-      const res = await axios.get(
-        `https://api.rawg.io/api/games?key=${
-          import.meta.env.VITE_RAWG_KEY
-        }&page=${page}`
-      );
-      const data = res.data.results;
-      setGames(data);
-      setFilteredGames(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const {
+    filteredGames,
+    select,
+    genres,
+    handleSelect,
+    filterGames,
+    handlePrev,
+    handleNext,
+  } = useFetch();
 
   return (
     <div className='m-4 rounded-xl flex gap-4 text-white'>
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import.meta.env.VITE_RAWG_KEY;
+
+const useFetch = () => {
+  const [games, setGames] = useState([]);
+  const [filteredGames, setFilteredGames] = useState([]);
+  const [page, setPage] = useState(1);
+  const [select, setSelect] = useState(0);
+  const genres = [
+    {
+      id: 2,
+      name: 'Shooter',
+    },
+    {
+      id: 3,
+      name: 'Action',
+    },
+    {
+      id: 4,
+      name: 'Adventure',
+    },
+    {
+      id: 5,
+      name: 'RPG',
+    },
+    {
+      id: 7,
+      name: 'Puzzle',
+    },
+    {
+      id: 51,
+      name: 'Indie',
+    },
+    {
+      id: 59,
+      name: 'Multiplayer',
+    },
+    {
+      id: 83,
+      name: 'Platformer',
+    },
+  ];
+
+  useEffect(() => {
+    fetchGames();
+  }, [page]);
+
+  const handleSelect = name => {
+    setSelect(name);
+  };
+
+  const filterGames = genre => {
+    const { id, name } = genre;
+    name === 'All'
+      ? setFilteredGames(games)
+      : setFilteredGames(
+          games.filter(
+            game => game.genres && game.genres.find(genre => genre.id === id)
+          )
+        );
+  };
+
+  const handlePrev = () => {
+    page > 0 && setPage(page => page - 1);
+  };
+
+  const handleNext = () => {
+    setPage(page => page + 1);
+  };
+
+  const fetchGames = async () => {
+    try {
+      const res = await axios.get(
+        `https://api.rawg.io/api/games?key=${
+          import.meta.env.VITE_RAWG_KEY
+        }&page=${page}`
+      );
+      const data = res.data.results;
+      setGames(data);
+      setFilteredGames(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return {
+    games,
+    filteredGames,
+    select,
+    genres,
+    handleSelect,
+    filterGames,
+    handlePrev,
+    handleNext,
+  };
+};
+
+export default useFetch;
